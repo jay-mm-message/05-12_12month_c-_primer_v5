64237829cc23156eb473f1a27793b9a6500569d0
jay-mm-message <jay.mm.message@gmail.com>
2025-12-05 21:07:42 +0800

    add warning example

---
+++ ch01/day01_hw_1_8.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	//cout << "/*" << endl; // correct
+	//cout << "*/" << endl; // correct
+	//cout << /* "*/" */  << endl;  // incorrect
+	//cout << /* "*/" /*  "*/" */  << endl; // incorrect
+	return 0;
+}
