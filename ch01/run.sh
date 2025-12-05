76c246d9b78cdb6c1d6da9e81657367db7082ac8
jay-mm-message <jay.mm.message@gmail.com>
2025-12-05 16:30:18 +0800

    add a check that the file name isn't null

--- ch01/run.sh
+++ ch01/run.sh
@@ -1,13 +1,16 @@
 
 clear
+if [ "$1" == "" ]; then
+	echo "please provide a file name"
+else
+	if [ -e $1 ]; then
+		g++ $1
 
-if [ -e $1 ]; then
-	g++ $1
-
-	if [ -e a.out ]; then
-		./a.out
-		echo "system state: $?" # display system state
-	else
-		echo "a.out not exist"
+		if [ -e a.out ]; then
+			./a.out
+			echo "system state: $?" # display system state
+		else
+			echo "a.out not exist"
+		fi
 	fi
-fi
+fi 
