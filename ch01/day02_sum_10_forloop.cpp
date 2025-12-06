6890aee89971b2da118ac89fa8408029a0e88861
jay-mm-message <jay.mm.message@gmail.com>
2025-12-06 14:57:05 +0800

    sum of 1 to 10 inclusive

---
+++ ch01/day02_sum_10_forloop.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// sum of 1 to 10
+	int sum = 0;
+	for(int i = 1 ; i <= 10 ; ++i) {
+		sum += i;
+	}
+	cout << "sum of 1 to 10 inclusive is "
+		 << sum
+		 << endl;
+
+	return 0;
+}
