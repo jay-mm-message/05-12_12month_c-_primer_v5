7e0ac81de62aa12f78d4c28f86d9c00a3b89b539
jay-mm-message <jay.mm.message@gmail.com>
2025-12-07 16:12:21 +0800

    Test interchange between int and unsigned in addition and subtraction

---
+++ ch02/ch02_hw_1_3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	unsigned u1 = 10, u2 = 42;
+	int i1 = 10, i2 = 42;
+
+	cout << i2 << "-" << i1 << ":" << i2 - i1 << endl;
+	cout << i1 << "-" << i2 << ":" << i1 - i2 << endl;
+	cout << u2 << "-" << u1 << ":" << u2 - u1 << endl;
+	cout << u1 << "-" << u2 << ":" << u1 - u2 << endl;
+	cout << i1 << "-" << u1 << ":" << i1 - u1 << endl;
+	cout << u1 << "-" << i1 << ":" << u1 - i1 << endl;
+
+ 	return 0;
+}
