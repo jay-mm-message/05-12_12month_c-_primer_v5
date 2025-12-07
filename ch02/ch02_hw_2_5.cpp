d4a9da8bd8ba92a0844741527ce26838e2047d06
jay-mm-message <jay.mm.message@gmail.com>
2025-12-07 19:23:48 +0800

    add hw 2.6

---
+++ ch02/ch02_hw_2_5.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << 'a' 	<< endl;
+	cout << L'a' 	<< endl;
+	cout << "a" 	<< endl;
+	cout << L"a" 	<< endl;
+
+	cout << endl;
+
+	cout << 10	<< endl;
+	cout << 10u	<< endl;
+	cout << 10ul	<< endl;
+	cout << 012 << endl;
+	cout << 0xa << endl;
+
+	cout << endl;
+
+	cout << 3.14 	<< endl;
+	cout << 3.14f	<< endl;
+	cout << 3.14L	<< endl;
+
+	cout << endl;
+
+	cout << 100		<< endl;
+	cout << 100u		<< endl;
+	cout << 100.		<< endl;
+	cout << 10e+1	<< endl;
+
+ 	return 0;
+}
