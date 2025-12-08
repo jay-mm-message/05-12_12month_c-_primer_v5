abb23cd001a8ca4a82549a33a19e8ddd07f5919f
jay-mm-message <jay.mm.message@gmail.com>
2025-12-08 15:49:12 +0800

    practice octal and decimal

---
+++ ch02/ch02_hw_2_6.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int month = 9, day = 7;
+	cout << "month: " << month << endl;
+	cout << "day: " << day << endl;
+	int month_a = 0, day_a = 0;
+	month_a = 011;
+	day_a = 07;
+	cout << "month_a: " << month_a << endl;
+	cout << "day_a: " << day_a << endl;
+	return 0;
+}
