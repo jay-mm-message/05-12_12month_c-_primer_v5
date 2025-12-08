a18b25caab279cbbceee6b2dc496de17a3a50e44
jay-mm-message <jay.mm.message@gmail.com>
2025-12-08 16:52:56 +0800

    practice reference variable

---
+++ ch02/ch02_hw_2_17.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int i = 42;
+int main() {
+	int i = 100;
+	int &ri = i;
+	ri = ri + 2;
+
+	int j = 45;
+	int *p = &j;
+	int &r2 = *p;
+
+	p = nullptr;
+	if (p) {
+		cout << "p is true" << endl;
+	} else {
+		cout << "p is nullptr" << endl;
+	}
+
+	// *p = 0;
+	// if (*p) {
+	// 	cout << "*p is true" << endl;
+	// } else {
+	// 	cout << "*p isn't true" << endl;
+	// }
+
+	cout << "r2 = " << r2 << endl;
+
+	cout << "i = " << i << endl;
+	cout << "ri = " << ri << endl;
+	return 0;
+}
